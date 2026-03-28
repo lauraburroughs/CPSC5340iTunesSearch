b409e4ac3a9f9b4763a392d6049e9d134a9788ec
Laura Burroughs <hellolauraburroughs@gmail.com>
2026-03-28 09:56:19 -0500

    Initial Commit

---
+++ iTunesSearch/iTunesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearchApp.swift
+//  iTunesSearch
+//
+//  Created by Laura Burroughs on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
